73d5f80739740c386a1e32d94a8cdf84df8cb03b
lazarocosta <lazaroosta@hotmail.com>
2016-11-17 17:20:53 +0000

    update

--- projeto2/primitives/Chessboard.js
+++ projeto2/primitives/Chessboard.js
@@ -7,32 +7,49 @@ function Chessboard(scene,divU, divV, textureref, color1, color2, colorMark, sel
 	this.scene =scene;
 	this.divU=divU;
 	this.divV=divV;
+	this.seleU = seleU;
+	this.seleV = seleV;
 
 this.plane = new Plane(scene, 1,1,this.divU,this.divV);
 
 this.texture = this.scene.graph.textures[textureref].file;
 this.shader = new CGFshader(this.scene.gl, "shaders/flat.vert", "shaders/flat.frag");
 
-
+//this.updateMark();
 this.shader.setUniformsValues({
-								 uSampler : 0,
-                                  divU:parseInt(divU)*1.0,
-								  divV:parseInt(divV)*1.0,
-                                  seleU:parseInt(seleU)*1.0,
-								  seleV:parseInt(seleV)*1.0,
+								  uSampler : 0,
+                                  divU:this.divU*1.0,
+								  divV:this.divV*1.0,
+                                  seleU:this.seleU*1.0,
+								  seleV:this.seleV*1.0,
 								  color1:color1,
 								  color2:color2,
 								  colorMark:colorMark
 								});
 
 
-
 };
 Chessboard.prototype = Object.create(CGFobject.prototype);
 Chessboard.prototype.constructor = Chessboard;
 
+Chessboard.prototype.updateMark = function (){
+	this.seleU++;
+	this.seleU %= this.divU;
+
+	if(this.seleU == 0)
+	this.seleV++;
+
+	if(this.seleV == this.divV)
+	this.seleV=0;
+}
+
 Chessboard.prototype.display = function () {
 
+	//this.updateMark();
+	this.shader.setUniformsValues({ 
+								  seleU:this.seleU*1.0,
+								  seleV:this.seleV*1.0
+								});
 	this.texture.bind(0);
 	 this.scene.setActiveShader(this.shader);
 	this.plane.display();
--- projeto2/primitives/Vehicle.js
+++ projeto2/primitives/Vehicle.js
@@ -3,8 +3,15 @@
  * @constructor
  */
 function Vehicle(scene) {
+
+
 };
 
  Vehicle.prototype = Object.create(CGFobject.prototype);
  Vehicle.prototype.constructor =  Vehicle;
 
+ Vehicle.prototype.display =function(){
+
+
+
+ }
