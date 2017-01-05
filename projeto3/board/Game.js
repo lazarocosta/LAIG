918c0963d29bbf97c67a0106bac120bfc52b0658
Diogo Luís Silva <digax96@gmail.com>
2017-01-05 14:31:11 +0000

    Updated Game

--- projeto3/board/Game.js
+++ projeto3/board/Game.js
@@ -15,31 +15,62 @@ function Game(id) {
 
 Game.prototype.constructor = Game;
 
-Game.prototype.getNewBoard = function () {
+Game.prototype.getNewBoard = function() {
     var boardTemp = this.client.sendRequest("newBoard");
+    this.setBoard(boardTemp);
+}
+
+Game.prototype.setBoard = function(boardTemp) {
     this.board = JSON.parse(boardTemp);
 }
 
-Game.prototype.begin = function () {
+Game.prototype.begin = function() {
     this.getNewBoard();
 }
 
-Game.prototype.stop = function () {
+Game.prototype.stop = function() {
     this.state = Game.GameState.End;
 }
 
-Game.prototype.getRedPoints = function () {
+Game.prototype.getRedPoints = function() {
     var tempPoints = this.client.sendRequest("redPoints(" + boardToString(this.board) + ")");
     this.redPoints = tempPoints;
 }
 
-Game.prototype.getWhitePoints = function () {
+Game.prototype.getWhitePoints = function() {
     var tempPoints = this.client.sendRequest("whitePoints(" + boardToString(this.board) + ")");
     this.redPoints = tempPoints;
 }
 
-Game.prototype.shutdown = function () {
+Game.prototype.shutdown = function() {
     console.log(this.client.sendRequest("quit"));
+}
+
+Game.prototype.move = function(Xi, Yi, Xf, Yf) {
+    var board = boardToString(this.board);
+    var dir = 0;
+    var dy = Yf - Yi;
+    var dx = Xf - Xi;
+    var Num = 0;
+    if (dy > 0) {
+        dir = 1;
+        Num = dy;
+    }
+    if (dy < 0) {
+        dir = 0;
+        Num = dy;
+    }
+    if (dx > 0) {
+        dir = 2;
+        Num = dx;
+    }
+    if (dx < 0) {
+        dir = 3;
+        Num = dx;
+    }
+    var message = "move(" + board + "," + Xi + "," + Yi + "," + dir + "," + Num + ")";
+    var boardTemp = this.client.sendRequest(message);
+    this.setBoard(boardTemp);
 }
 
 Game.GameState = {
